3e2c21d05690f6d57fca5dac2d9d6a836bae3698
Lionel Acosta <lionelx87@gmail.com>
2023-05-10 23:14:46 -0300

    feat(service): create user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './users/users.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, UsersModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsersModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(public userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => console.log(data));
   }
-
 }
