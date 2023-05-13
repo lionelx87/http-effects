e3b424f4bd3c361912edfa0c0d1d01e93cb8c16d
Lionel Acosta <lionelx87@gmail.com>
2023-05-13 00:55:08 -0300

    feat(store): template user

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
 import { loadUser } from 'src/app/store/actions';
 import { AppState } from 'src/app/store/app.reducers';
 
@@ -10,9 +11,18 @@ import { AppState } from 'src/app/store/app.reducers';
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  user: User | null;
+  loading: boolean = false;
+  error: any;
   constructor(private router: ActivatedRoute, private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.select('user').subscribe(({ user, loading, error }) => {
+      this.user = user;
+      this.loading = loading;
+      this.error = error;
+    });
+
     this.router.params.subscribe(({ id }) => {
       console.log(id);
       this.store.dispatch(loadUser({ id }));
